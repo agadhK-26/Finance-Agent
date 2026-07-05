12880344378923cac2c59dc2043195a57f9c2ff3
agadhK-26 <agadh2000k@gmail.com>
2026-07-06 01:14:55 +0530

    Update database.py

--- storage/database.py
+++ storage/database.py
@@ -1,3 +1,4 @@
+
 import sqlite3
 
 DB_NAME = "memory.db"
@@ -8,8 +9,9 @@ def init_db():
     c = conn.cursor()
 
     c.execute("""
-    CREATE TABLE IF NOT EXISTS messages (
+    CREATE TABLE IF NOT EXISTS messages(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id TEXT,
         session_id TEXT,
         role TEXT,
         content TEXT
@@ -20,43 +22,49 @@ def init_db():
     conn.close()
 
 
-def save_message(session_id, role, content):
+def save_message(user_id, session_id, role, content):
     conn = sqlite3.connect(DB_NAME)
     c = conn.cursor()
 
-    c.execute(
-        "INSERT INTO messages (session_id, role, content) VALUES (?, ?, ?)",
-        (session_id, role, content)
-    )
+    c.execute("""
+    INSERT INTO messages(user_id,session_id,role,content)
+    VALUES(?,?,?,?)
+    """,(user_id,session_id,role,content))
 
     conn.commit()
     conn.close()
 
 
-def load_messages(session_id):
+def load_messages(user_id, session_id):
     conn = sqlite3.connect(DB_NAME)
     c = conn.cursor()
 
-    c.execute(
-        "SELECT role, content FROM messages WHERE session_id=? ORDER BY id",
-        (session_id,)
-    )
+    c.execute("""
+    SELECT role,content
+    FROM messages
+    WHERE user_id=? AND session_id=?
+    ORDER BY id
+    """,(user_id,session_id))
 
-    rows = c.fetchall()
-
-    conn.close()
-
-    return [{"role": r, "content": c} for r, c in rows]
-
-
-def get_sessions():
-    conn = sqlite3.connect(DB_NAME)
-    c = conn.cursor()
-
-    c.execute("SELECT DISTINCT session_id FROM messages")
-
-    sessions = [x[0] for x in c.fetchall()]
+    rows=c.fetchall()
 
     conn.close()
 
-    return sessions
+    return [{"role":r,"content":c} for r,c in rows]
+
+
+def get_sessions(user_id):
+    conn=sqlite3.connect(DB_NAME)
+    c=conn.cursor()
+
+    c.execute("""
+    SELECT DISTINCT session_id
+    FROM messages
+    WHERE user_id=?
+    """,(user_id,))
+
+    rows=[x[0] for x in c.fetchall()]
+
+    conn.close()
+
+    return rows
